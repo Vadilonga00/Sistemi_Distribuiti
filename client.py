06bedf475b11beb53f3e863f4c1d922fd5d0f0c8
MariaElenaContini <con.elena@hotmail.it>
2022-06-15 12:20:42 +0200

    inizializzazione del prompt

--- client.py
+++ client.py
@@ -1,5 +1,20 @@
 import socket
 from sys import argv
+from cmd import Cmd
+
+def managePrompt(prompt):
+    prompt.cmdloop()
+
+class myPrompt(Cmd):
+    prompt = '>'
+    intro = 'Ciao, benvenuto nel mio prompt:'
+
+    def _init_(self):
+        super()._init_()
+        self.socket = clientsocket
+        self.is_connect = True
+        self.topic_message = {}
+        self.threading = []
 
 if __name__ == '__main__':
     myIP = socket.gethostbyname(socket.gethostname())
@@ -10,3 +25,6 @@ if __name__ == '__main__':
     clientsocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM) #viene letta dal cmd?
     clientsocket.bind((myIP, myPORT))
     clientsocket.connect((brokerIP, brokerPort))
+    
+    prompt = myPrompt()
+    prompt.cmdloop()
