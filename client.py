927b3c85421209d0110155705f8e4a8ec21731eb
francescocarpineti091299 <francesco.carpineti@alcampus.it>
2022-06-15 12:23:25 +0200

    connection functions added

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 from sys import argv
 from cmd import Cmd
+from threading import Thread
 
 def managePrompt(prompt):
     prompt.cmdloop()
@@ -15,6 +16,31 @@ class myPrompt(Cmd):
         self.is_connect = True
         self.topic_message = {}
         self.threading = []
+
+    @staticmethod
+    def do_ciao(inp):
+        print('Ciao a te')
+
+    def do_showtopic(self, inp):
+        print(self.topic_message)
+
+    def do_connect_tcp(self, inp):
+        if not self.is_connect:
+            args = inp.split(" ")
+            address = args[0]
+            port = int(args[1])
+            self.socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+            self.socket.connect((address, port))
+            self.is_connect = True
+
+    def do_connect_to_broker(self, inp):
+        if self.is_connect:
+            messaggio = '[CONNECT]'
+            self._sendall2(messaggio)
+            self.threading = Thread(target=self._receive_message, args=(self.socket,))
+            self.threading.start()
+            print(self.threading)
+
 
 if __name__ == '__main__':
     myIP = socket.gethostbyname(socket.gethostname())
