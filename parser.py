fed064cafc26e7e1c725f32f02c4acdcbbc87c9a
francescocarpineti091299 <francesco.carpineti@alcampus.it>
2022-06-20 13:12:24 +0200

    parser fix

--- parser.py
+++ parser.py
@@ -3,15 +3,15 @@ import argparse
 def initialize_parser():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-i1", "client_port",
+    parser.add_argument("client_port",
                         help="The port of your device",
                         type=int)
 
-    parser.add_argument("-i2", "broker_ip",
+    parser.add_argument("broker_ip",
                         help="The IP address of the broker",
                         type=str)
 
-    parser.add_argument("-i3", "broker_port",
+    parser.add_argument("broker_port",
                         help="The port of the broker",
                         type=int)
 
